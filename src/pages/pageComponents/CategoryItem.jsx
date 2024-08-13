c9dceb8746ed5b205352a562eb7c0495f5c0f5a6
nurdawletkenesbaev <nurdawletkenesbaev@gmail.com>
2024-08-13 21:52:15 +0500

    salkr

--- src/pages/pageComponents/CategoryItem.jsx
+++ src/pages/pageComponents/CategoryItem.jsx
@@ -77,8 +77,8 @@ const CategoryItem = ({ item }) => {
             <div className='absolute z-10 top-10 p-[20px] bg-white h-auto border-[1px] border-gray-300 shadow-lg rounded-sm'>
               <p>Are you sure this category deleted?</p>
               <div className='flex justify-end gap-3 pt-3'>
-                <button onClick={() => setModal(false)} className='py-[7px] px-[20px] bg-indigo-500 text-white rounded-sm border-gray-300 border-[1px]'>Cancel</button>
-                <button onClick={() => deleteItem(url, item.id)} className='py-[7px] px-[20px] rounded-sm bg-red-500 text-white'>Delete</button>
+                <button onClick={() => setModal(false)} className='py-[7px] px-[20px] bg-indigo-500 text-white rounded-sm border-gray-300 border-[1px] active:scale-95'>Cancel</button>
+                <button onClick={() => deleteItem(url, item.id)} className='py-[7px] px-[20px] rounded-sm bg-red-500 text-white active:scale-95'>Delete</button>
               </div>
             </div>
             :
